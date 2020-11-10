d05e0718b2b47221eec02f0224428a687d13936a
Alexey Makarov <makarov.lesha@gmail.com>
2020-11-10 12:38:10 +0100

    Lesson 5 Homework finished

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -3,21 +3,29 @@ import Heading from '../Heading/Heading';
 
 import s from './PokemonCard.module.scss';
 
-const PokemonCard = () => {
-  console.log('PokemonCard -> s.titleName', s.titleName);
+import { IPokemons } from '../../pages/Pokedex/pokemons';
+
+interface IPokemonProps {
+  pokemonParams: IPokemons;
+  key?: any;
+}
+
+const PokemonCard: React.FC<IPokemonProps> = ({ pokemonParams }) => {
+  console.log('pokemonParams', pokemonParams);
+
   return (
     <div className={s.root}>
       <div className={s.infoWrap}>
         <Heading level="h6" className={s.titleName}>
-          Charmander
+          {pokemonParams.name}
         </Heading>
         <div className={s.statWrap}>
           <div className={s.statItem}>
-            <div className={s.statValue}>52</div>
+            <div className={s.statValue}> {pokemonParams.stats.attack}</div>
             Attack
           </div>
           <div className={s.statItem}>
-            <div className={s.statValue}>43</div>
+            <div className={s.statValue}>{pokemonParams.stats.defense}</div>
             Defense
           </div>
         </div>
@@ -26,10 +34,7 @@ const PokemonCard = () => {
         </div>
       </div>
       <div className={s.pictureWrap}>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
-          alt="Charmander"
-        />
+        <img src={pokemonParams.img} alt={pokemonParams.name} />
       </div>
     </div>
   );
--- src/pages/Pokedex/PokedexPage.tsx
+++ src/pages/Pokedex/PokedexPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Layout from '../../components/Layout/index';
 import PokemonCard from '../../components/PokemonCard';
+import pokemons from './pokemons';
 
 import s from './PokedexPage.module.scss';
 
@@ -11,10 +12,9 @@ const PokedexPage = () => {
       <Header />
       <Layout>
         <div className={s.root}>
-          <PokemonCard />
-          <PokemonCard />
-          <PokemonCard />
-          <PokemonCard />
+          {pokemons.map((item, index) => (
+            <PokemonCard key={item.name} pokemonParams={item} />
+          ))}
         </div>
       </Layout>
     </>
