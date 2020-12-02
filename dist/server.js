d682094f59a60d4ba36e9fb619590255bc0c392f
Alexey Makarov <makarov.lesha@gmail.com>
2020-12-02 14:39:09 +0100

    lesson 9.03 done

---
+++ dist/server.js
@@ -0,0 +1,124 @@
+/* eslint-disable*/
+/******/ (function (modules) {
+  // webpackBootstrap
+  /******/ // The module cache
+  /******/ var installedModules = {}; // The require function
+  /******/
+  /******/ /******/ function __webpack_require__(moduleId) {
+    /******/
+    /******/ // Check if module is in cache
+    /******/ if (installedModules[moduleId]) {
+      /******/ return installedModules[moduleId].exports;
+      /******/
+    } // Create a new module (and put it into the cache)
+    /******/ /******/ var module = (installedModules[moduleId] = {
+      /******/ i: moduleId,
+      /******/ l: false,
+      /******/ exports: {},
+      /******/
+    }); // Execute the module function
+    /******/
+    /******/ /******/ modules[moduleId].call(module.exports, module, module.exports, __webpack_require__); // Flag the module as loaded
+    /******/
+    /******/ /******/ module.l = true; // Return the exports of the module
+    /******/
+    /******/ /******/ return module.exports;
+    /******/
+  } // expose the modules object (__webpack_modules__)
+  /******/
+  /******/
+  /******/ /******/ __webpack_require__.m = modules; // expose the module cache
+  /******/
+  /******/ /******/ __webpack_require__.c = installedModules; // define getter function for harmony exports
+  /******/
+  /******/ /******/ __webpack_require__.d = function (exports, name, getter) {
+    /******/ if (!__webpack_require__.o(exports, name)) {
+      /******/ Object.defineProperty(exports, name, { enumerable: true, get: getter });
+      /******/
+    }
+    /******/
+  }; // define __esModule on exports
+  /******/
+  /******/ /******/ __webpack_require__.r = function (exports) {
+    /******/ if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+      /******/ Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+      /******/
+    }
+    /******/ Object.defineProperty(exports, '__esModule', { value: true });
+    /******/
+  }; // create a fake namespace object // mode & 1: value is a module id, require it // mode & 2: merge all properties of value into the ns // mode & 4: return value when already ns object // mode & 8|1: behave like require
+  /******/
+  /******/ /******/ /******/ /******/ /******/ /******/ __webpack_require__.t = function (value, mode) {
+    /******/ if (mode & 1) value = __webpack_require__(value);
+    /******/ if (mode & 8) return value;
+    /******/ if (mode & 4 && typeof value === 'object' && value && value.__esModule) return value;
+    /******/ var ns = Object.create(null);
+    /******/ __webpack_require__.r(ns);
+    /******/ Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+    /******/ if (mode & 2 && typeof value != 'string')
+      for (var key in value)
+        __webpack_require__.d(
+          ns,
+          key,
+          function (key) {
+            return value[key];
+          }.bind(null, key),
+        );
+    /******/ return ns;
+    /******/
+  }; // getDefaultExport function for compatibility with non-harmony modules
+  /******/
+  /******/ /******/ __webpack_require__.n = function (module) {
+    /******/ var getter =
+      module && module.__esModule
+        ? /******/ function getDefault() {
+            return module['default'];
+          }
+        : /******/ function getModuleExports() {
+            return module;
+          };
+    /******/ __webpack_require__.d(getter, 'a', getter);
+    /******/ return getter;
+    /******/
+  }; // Object.prototype.hasOwnProperty.call
+  /******/
+  /******/ /******/ __webpack_require__.o = function (object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
+  }; // __webpack_public_path__
+  /******/
+  /******/ /******/ __webpack_require__.p = ''; // Load entry module and return exports
+  /******/
+  /******/
+  /******/ /******/ return __webpack_require__((__webpack_require__.s = './src/server/server.js'));
+  /******/
+})(
+  /************************************************************************/
+  /******/ {
+    /***/ './src/server/server.js':
+      /*!******************************!*\
+  !*** ./src/server/server.js ***!
+  \******************************/
+      /*! no static exports found */
+      /***/ function (module, exports, __webpack_require__) {
+        'use strict';
+        eval(
+          "\r\nvar __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {\r\n    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }\r\n    return new (P || (P = Promise))(function (resolve, reject) {\r\n        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }\r\n        function rejected(value) { try { step(generator[\"throw\"](value)); } catch (e) { reject(e); } }\r\n        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }\r\n        step((generator = generator.apply(thisArg, _arguments || [])).next());\r\n    });\r\n};\r\nconst Hapi = __webpack_require__(/*! @hapi/hapi */ \"@hapi/hapi\");\r\nconst init = () => __awaiter(void 0, void 0, void 0, function* () {\r\n    const server = Hapi.server({\r\n        port: 3000,\r\n        host: 'localhost',\r\n    });\r\n    server.route({\r\n        method: 'GET',\r\n        path: '/{any*}',\r\n        handler: (request) => {\r\n            return `Hello World! This is page ${request.path}`;\r\n        },\r\n    });\r\n    yield server.start();\r\n    console.log('Server running on %s', server.info.uri);\r\n});\r\nprocess.on('unhandledRejection', (err) => {\r\n    console.log(err);\r\n    process.exit(1);\r\n});\r\ninit();\r\n\n\n//# sourceURL=webpack:///./src/server/server.js?",
+        );
+
+        /***/
+      },
+
+    /***/ '@hapi/hapi':
+      /*!*****************************!*\
+  !*** external "@hapi/hapi" ***!
+  \*****************************/
+      /*! no static exports found */
+      /***/ function (module, exports) {
+        eval('module.exports = require("@hapi/hapi");\n\n//# sourceURL=webpack:///external_%22@hapi/hapi%22?');
+
+        /***/
+      },
+
+    /******/
+  },
+);
